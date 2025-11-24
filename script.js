0271acea4ca0a344bd766fca0d2b1879a25c268d
najamecheri <najamecheri28@gmail.com>
2025-11-24 20:38:06 +0530

    Initial commit of portfolio website

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add any interactivity here, for example:
+
+// Mobile menu toggle
+// const nav = document.querySelector('nav ul');
+// const menuToggle = document.createElement('button');
+// menuToggle.textContent = 'Menu';
+// document.querySelector('header').prepend(menuToggle);
+
+// menuToggle.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+// });
